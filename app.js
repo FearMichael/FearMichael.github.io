0765b1c6170825f4024ed4b2b2047b1223606cf7
Starry Night <44407940+FearMichael@users.noreply.github.com>
2019-04-08 17:10:07 -0400

    edited pic, removed contact, added email button, and css tweaks

--- app.js
+++ app.js
@@ -1,18 +1,34 @@
 M.AutoInit();
 
-// var instance = M.Carousel.init({
-//     fullWidth: true,
-//     indicators: true,
+const sectionContent = $(".sectionContent");
+const sectionList = $(".sectionList");
+// const emailButton = $(".emailButton");
 
+// emailButton.click(function(event) {
+//     let name = $(".emailName").val().trim();
+//     let content = $(".emailBody").val().trim();
+//     let email = $(".email").val().trim();
+//     let form = {email, name, content}
+//     console.log(name, email, content);
+//     $.post("https://mbhp5d3aud.execute-api.us-east-1.amazonaws.com/dev/email/send", form, function(data) {
+//         console.log(data);
+//     });
 // });
 
-// document.addEventListener('DOMContentLoaded', function() {
-//     var elems = document.querySelectorAll('.carousel');
-    
-//     var instances = M.Carousel.init(elems, options);
-//   });
+screenSizeAdj = () => {
+    if ($(window).width() < 990) {
+        sectionContent.addClass("center-align");
+        sectionList.addClass("narrowList");
+    } else if ($(window).width() < 990) {
+        sectionContent.removeClass("center-align");
+        sectionList.removeClass("narrowList");
 
-// $(".carousel").carousel({
-//     fullWidth: false,
-//     indicators: true
-// });
+    }
+};
+
+$(document).ready(function() {
+    screenSizeAdj();
+    $(window).resize(function() {
+        screenSizeAdj();
+    });
+});
