18983c44ebc19968008845e2656ab2a4adc5446c
Starry Night <44407940+FearMichael@users.noreply.github.com>
2019-04-14 10:48:38 -0400

    cosmetics

--- app.js
+++ app.js
@@ -2,14 +2,17 @@ M.AutoInit();
 
 const sectionContent = $(".sectionContent");
 const sectionList = $(".sectionList");
+const title = $(".titleButtons");
 
 screenSizeAdj = () => {
     if ($(window).width() < 990) {
         sectionContent.addClass("center-align");
         sectionList.addClass("narrowList");
+        title.removeClass("valign-wrapper");
     } else {
         sectionContent.removeClass("center-align");
         sectionList.removeClass("narrowList");
+        title.addClass("valign-wrapper");
     }
 };
 
