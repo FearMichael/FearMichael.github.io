2273ee6f9a14a2ee6afec77bef21bbb767d9e7ed
Michael Fearnley <44407940+FearMichael@users.noreply.github.com>
2019-07-17 16:33:00 -0400

    Updates

---
+++ precache-manifest.196346215fed40980f25b29358c4472e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb88903beff60870f995ff539f520fbb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8ad84c98851d0861472a",
+    "url": "/static/css/2.ad9eb4b5.chunk.css"
+  },
+  {
+    "revision": "39ad2771d1268e405c1c",
+    "url": "/static/css/main.81ce16ca.chunk.css"
+  },
+  {
+    "revision": "8ad84c98851d0861472a",
+    "url": "/static/js/2.dca7a3fb.chunk.js"
+  },
+  {
+    "revision": "39ad2771d1268e405c1c",
+    "url": "/static/js/main.94483df6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
