11f27b7175659f167c9107a88fae0b06c9a26eb3
Michael Fearnley <44407940+FearMichael@users.noreply.github.com>
2019-10-27 21:10:04 -0400

    picture sizes

--- src/portfolioInfo.js
+++ src/portfolioInfo.js
@@ -1,7 +1,7 @@
 const portfolioInfo = [
     {
         name: "Bench Strength",
-        picture: "./images/benchstrength.png",
+        picture: "./images/benchstrength.jpg",
         description: "A group curated app that provides Team Leaders the ability to assess employee skills and interests to build smarter, more invested teams. My primary responsibilities revolved around building front end components as well as integrating Github authentication via Auth0.",
         tools: ["Angular 7", "Typescript", "Bootstrap", "Sequelize", "Auth0", "AWS", "Express API Server"],
         deployedLink: "http://ec2-3-80-218-219.compute-1.amazonaws.com:3000/",
@@ -9,7 +9,7 @@ const portfolioInfo = [
     },
     {
         name: "Daily Dash",
-        picture: "./images/dailydash.png",
+        picture: "./images/dailydash.jpg",
         description: "Ever been annoyed at needing to go several different apps or websites to retrieve daily information? DailyDash, a team built application, consolidates all of that information into a personalized experience for you. I was responsible for authentication using Passport with a Google Provider along with the API server and data structures. I also resolved time displaying properly on the front end.",
         tools: ["Sequelize", "Heroku", "Materialize", "Node.js", "Passportjs"],
         deployedLink: "http://dailydashboardproject2.herokuapp.com/",
@@ -25,7 +25,7 @@ const portfolioInfo = [
     },
     {
         name: "Giftastic",
-        picture: "./images/gifTastic.png",
+        picture: "./images/gifTastic.jpg",
         description: "This fun web app will provide the user with 10 gifs as well as creating a button for easy re-use.",
         tools: ["Giphy API", "Github", "Materialize", "jQuery"],
         deployedLink: "https://fearmichael.github.io/giftastic/",
@@ -33,7 +33,7 @@ const portfolioInfo = [
     },
     {
         name: "Train Tracker",
-        picture: "./images/trainTracker.png",
+        picture: "./images/trainTracker.jpg",
         description: "This is a prototype for tracking train schedules. This utilizes a NoSQL database (Firebase) along with authentication.",
         tools: ["Firebase", "Moment JS", "Materialize", "User input validation"],
         deployedLink: "https://traintracker-e3faf.firebaseapp.com/",
@@ -41,7 +41,7 @@ const portfolioInfo = [
     },
     {
         name: "Word Guess Game",
-        picture: "./images/wordGuess.png",
+        picture: "./images/wordGuess.jpg",
         description: "A fun photography themed word guess game.",
         tools: ["Input validation", "Github Version Control", "Bootstrap", "jQuery"],
         deployedLink: "https://fearmichael.github.io/word-guess-game/",
