a88c81545604f9a2479a694fab96e492d5e08a77
Michael Fearnley <44407940+FearMichael@users.noreply.github.com>
2022-02-07 22:32:11 -0500

    add new resume

--- src/components/About.js
+++ src/components/About.js
@@ -33,7 +33,7 @@ class About extends Component {
                                 <hr className="line"></hr>
                                 <a href="https://github.com/FearMichael" target="_blank" rel="noopener noreferrer"><img className="logoimg" src="images/github-logo.png" alt="github logo" /></a>
                                 <a href="https://www.linkedin.com/in/michaelfearnley/" target="_blank" rel="noopener noreferrer"><img className="logoimg" src="images/linkedin-logo.png" alt="linked in logo" /></a>
-                                <a href="./MichaelFearnleyDeveloperResume2019.pdf" target="_blank" rel="noopener noreferrer">
+                                <a href="./Michael_Fearnley_SoftwareEngineer.pdf" target="_blank" rel="noopener noreferrer">
                                     {/* file icon */}
                                     <svg className="logoimg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M4.01 2L4 22h16V8l-6-6H4.01zM13 9V3.5L18.5 9H13z" /></svg>
                                 </a>
